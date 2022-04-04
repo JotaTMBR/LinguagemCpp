750a13f8167aa6aea03db2e3bcb7576be4f88586
Joao <joaosema.alves123@gmail.com>
2022-04-04 08:52:32 -0300

    Estudo C++

---
+++ 1-010420222/primeiro.cpp
@@ -0,0 +1,11 @@
+//Programa para exibiruma mensagem. O código foi escrito em C++
+//Importação da biblioteca de entrada e saida de dados
+#include <iostream>
+
+int main(){
+    //saida padrao de dados
+    //cont(console out  -> saida da tela de console(terminar))
+    std::cout << "Bem vindo ao C++\n";
+
+    return 0;
+}
